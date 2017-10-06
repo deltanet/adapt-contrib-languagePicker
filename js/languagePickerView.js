79ea80f154037b0e54296da5fdf48d4be1802e1c
RobertPeek <robert@delta-net.co.uk>
2017-10-06 10:27:44 +0100

    Adds inrow functionality Fixes #4

--- js/languagePickerView.js
+++ js/languagePickerView.js
@@ -2,22 +2,22 @@ define([
     'core/js/adapt',
     './accessibilityView'
 ], function(Adapt, accessibilityView) {
-    
+
     var LanguagePickerView = Backbone.View.extend({
-        
+
         events: {
             'click .languagepicker-languages button': 'onLanguageClick'
         },
-        
+
         className: 'languagepicker',
-        
+
         initialize: function () {
             this.initializeAccessibility();
             $("html").addClass("in-languagepicker");
             this.listenTo(Adapt, 'remove', this.remove);
             this.render();
         },
-        
+
         render: function () {
             var data = this.model.toJSON();
             var template = Handlebars.templates[this.constructor.template];
@@ -25,16 +25,16 @@ define([
             this.$el.addClass(data._classes);
 
             document.title = this.model.get('title') || "";
-            
+
             _.defer(_.bind(function () {
                 this.postRender();
             }, this));
         },
-        
+
         postRender: function () {
             $('.loading').hide();
         },
-        
+
         onLanguageClick: function (event) {
             this.destroyAccessibility();
             this.model.setLanguage($(event.target).val());
@@ -44,7 +44,7 @@ define([
             this.accessibilityView = new accessibilityView({
                 model:this.model
             });
-            
+
             // we need to re-render if accessibility gets switched on
             this.listenTo(this.accessibilityView, 'accessibility:toggle', this.render);
         },
@@ -58,7 +58,7 @@ define([
 
             Backbone.View.prototype.remove.apply(this, arguments);
         }
-        
+
     }, {
         template: 'languagePickerView'
     });
